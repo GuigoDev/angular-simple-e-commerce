0b9829b648d3a584eb663605f77cc34c090cd92e
GuigoDev <guilhermeazevedo.deb@gmail.com>
2022-04-12 12:16:10 -0300

    Template de detalhes de produto.

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Product, products } from '../products';
 
 @Component({
   selector: 'app-product-details',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailsComponent implements OnInit {
 
-  constructor() { }
+  product: Product | undefined;
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    // Obtem o  id do produto atual.
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+
+    // Encontra o produto que corresponde ao id fornecido na rota.
+    this.product = products.find(product => product.id === productIdFromRoute);
   }
 
 }
